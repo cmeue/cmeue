f8d6040790de2262be9ec73150d090dd30e86ccb
Rajat <razat249@gmail.com>
2016-10-04 10:35:38 +0530

    create an initial directory structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Layout from "./containers/Layout/Layout";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Layout />,
+  document.getElementById("root")
 );
