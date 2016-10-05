1c2327120249e0f3ff71073b1452b393b12b8f3c
Rajat <razat249@gmail.com>
2016-10-05 09:58:47 +0530

    add a basic navbar

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "../../components/layout/NavBar/NavBar";
 import Footer from "../../components/layout/Footer/Footer";
+import "bootstrap/dist/js/bootstrap.js";
 
 class Layout extends React.Component {
     render() {
