3aad8daea8a34a22677f96f3f5910ab3f92c85af
ishancoder <isaxena171@gmail.com>
2016-10-07 11:14:55 +0530

    add List, ListElement, BreadCrumbs component

---
+++ src/components/BreadCrumbs/BreadCrumbs.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./BreadCrumbsStyles.css";
+
+class BreadCrumb extends React.Component {
+    render() {
+        let rows = [];
+        let i;
+        for (i = 0; i < this.props.directory.length - 1; i++) {
+            rows.push(
+                <li key={i}>
+                    <a href="#">{this.props.directory[i]}</a>
+                </li>
+            );
+        }
+        rows.push(
+            <li key={i} className="active">{this.props.directory[i]}</li>
+        );
+        return (
+            <ol className="breadcrumb no-margin-breadcrumb">
+                {rows}
+            </ol>
+        );
+    }
+}
+
+export default BreadCrumb;
