1fbe022549c3449d1378d0654fc13b8939275a30
Atul Sharma <theatsharma@gmail.com>
2016-10-07 10:48:36 +0530

    Beutify footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -9,12 +9,21 @@ class Footer extends React.Component {
         return (
             <div className="footer">
                 <div className="container">
-                    <span><a href="#"><img className="footer-logo" src={logo} /></a>© 2016 CMEUE.</span>
+                    <span>
+                        <a href="#"><img className="footer-logo" src={logo}/></a>© 2016 CMEUE.</span>
                     <p className="footer-block">
-                        <span className="footer-menu-link"><a href="#" target="_blank">Terms</a></span>
-                        <span className="footer-menu-link"><a href="#" target="_blank">Privacy</a></span>
-                        <span className="footer-menu"><a href="#" target="_blank">Blog</a></span>
-                        <span className="footer-menu"><a href="#" target="_blank">About</a></span>
+                        <span className="footer-menu-link">
+                            <a href="#" target="_blank">Terms</a>
+                        </span>
+                        <span className="footer-menu-link">
+                            <a href="#" target="_blank">Privacy</a>
+                        </span>
+                        <span className="footer-menu">
+                            <a href="#" target="_blank">Blog</a>
+                        </span>
+                        <span className="footer-menu">
+                            <a href="#" target="_blank">About</a>
+                        </span>
                     </p>
                 </div>
             </div>
