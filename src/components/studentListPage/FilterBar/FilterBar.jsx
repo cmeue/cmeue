56c91d5eb302783d5ca7a7ebe98e9bf724ccecf6
Atul Sharma <theatsharma@gmail.com>
2016-10-21 00:27:45 +0530

    improve filter bar UI

--- src/components/studentListPage/FilterBar/FilterBar.jsx
+++ src/components/studentListPage/FilterBar/FilterBar.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
 import "./FilterBarStyles.css";
 
 class FilterBar extends React.Component {
@@ -8,55 +6,65 @@ class FilterBar extends React.Component {
         return (
             <div className="student-page-filter-bar">
                 <div className="filter-bar-college btn-group dropup">
-                    <span className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span>
-                            <i className="fa fa-filter" aria-hidden="true"></i> Branch
-                        </span>
-                    </span>
+                    <button type="button" className="btn btn-default student-page-filter-bar-btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span><i className="fa fa-filter" aria-hidden="true"></i>&nbsp;Branch</span>
+                    </button>
                     <ul className="dropdown-menu">
                         <li>
-                            <a href="#" data-value="option1" tabIndex="-1">
-                                <input type="checkbox"/>&nbsp;CSE
-                            </a>
+                            <div className="checkbox filter-bar-checkbox col-md-12">
+                                <label>
+                                    <input type="checkbox" value="CSE"/>&nbsp;Computer Science
+                                </label>
+                            </div>
                         </li>
                         <li>
-                            <a href="#" data-value="option1" tabIndex="-1">
-                                <input type="checkbox"/>&nbsp;IT
-                            </a>
+                            <div className="checkbox filter-bar-checkbox col-md-12">
+                                <label>
+                                    <input type="checkbox" />&nbsp;Information Technology
+                                </label>
+                            </div>
                         </li>
-                    </ul>
+                  </ul>
                 </div>
                 <div className="filter-bar-year btn-group dropup">
-                    <span className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span>
-                            <i className="fa fa-filter" aria-hidden="true"></i> Year
-                        </span>
-                    </span>
+                    <button type="button" className="btn btn-default student-page-filter-bar-btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span><i className="fa fa-filter" aria-hidden="true"></i>&nbsp;College</span>
+                    </button>
                     <ul className="dropdown-menu">
                         <li>
-                            <a href="#" data-value="option1" tabIndex="-1">
-                                <input type="checkbox"/>&nbsp;1<sup>st</sup> Year
-                            </a>
+                            <div className="checkbox filter-bar-checkbox col-md-12">
+                                <label>
+                                    <input type="checkbox" value="CSE"/>&nbsp;1<sup>st</sup>Year
+                                </label>
+                            </div>
                         </li>
                         <li>
-                            <a href="#" data-value="option1" tabIndex="-1">
-                                <input type="checkbox"/>&nbsp;2<sup>nd</sup> Year
-                            </a>
+                            <div className="checkbox filter-bar-checkbox col-md-12">
+                                <label>
+                                    <input type="checkbox" value="CSE"/>&nbsp;2<sup>nd</sup>Year
+                                </label>
+                            </div>
                         </li>
                         <li>
-                            <a href="#" data-value="option1" tabIndex="-1">
-                                <input type="checkbox"/>&nbsp;3<sup>rd</sup> Year
-                            </a>
+                            <div className="checkbox filter-bar-checkbox col-md-12">
+                                <label>
+                                    <input type="checkbox" value="CSE"/>&nbsp;3<sup>rd</sup>Year
+                                </label>
+                            </div>
                         </li>
                         <li>
-                            <a href="#" data-value="option1" tabIndex="-1">
-                                <input type="checkbox"/>&nbsp;4<sup>th</sup> Year
-                            </a>
+                            <div className="checkbox filter-bar-checkbox col-md-12">
+                                <label>
+                                    <input type="checkbox" value="CSE"/>&nbsp;4<sup>th</sup>Year
+                                </label>
+                            </div>
                         </li>
                         <li>
-                            <a href="#" data-value="option1" tabIndex="-1">
-                                <input type="checkbox"/>&nbsp;Alumni
-                            </a>
+                            <div className="checkbox filter-bar-checkbox col-md-12">
+                                <label>
+                                    <input type="checkbox" value="CSE"/>&nbsp;Alumni
+                                </label>
+                            </div>
                         </li>
                     </ul>
                 </div>
